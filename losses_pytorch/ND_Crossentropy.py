dcceaedd4ddf8519df8ce5649d1aad86b92205f3
yy4551 <100992443+yy4551@users.noreply.github.com>
2024-03-31 11:16:15 +0800

    今天要把创新点定下来

--- losses_pytorch/ND_Crossentropy.py
+++ losses_pytorch/ND_Crossentropy.py
@@ -37,7 +37,7 @@ class TopKLoss(CrossentropyND):
     """
     def __init__(self, weight=None, ignore_index=-100, k=10):
         self.k = k
-        super(TopKLoss, self).__init__(weight, False, ignore_index, reduce=False)
+        super(TopKLoss, self).__init__(weight, None, ignore_index, reduce='sum')
 
     def forward(self, inp, target):
         target = target[:, 0].long()
